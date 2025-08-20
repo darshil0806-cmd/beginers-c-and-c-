2566836b35c74e7ce269a0b4d2283e01a074fd22
Darshil Patel <darshil0806@gmail.com>
2025-08-20 21:14:26 +0530

    Add files via upload

---
+++ primeno_calculator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number to check if it's prime: ");
+    scanf("%d", &n);
+    int prime = 1;
+    if(n==1 || n==0){
+        printf("%d is not a prime number.\n", n);
+    }
+    else{
+    for (int i = 2; i <= n ; i++)
+    {
+        if (n / i == 0)
+        {
+            prime = 0;
+            break;
+        }
+    }
+    if (prime == 1)
+        printf("%d is a prime number.\n", n);
+    else
+        printf("%d is not a prime number.\n", n);
+    }
+    return 0;
+}
