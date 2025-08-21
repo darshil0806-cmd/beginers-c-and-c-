2aea395cb1636803c2e2f68a803b2180ac6f9258
Darshil Patel <darshil0806@gmail.com>
2025-08-21 14:38:47 +0530

    Add files via upload

---
+++ natural_no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int i = 0;
+int n;
+printf("how many natural no. do you want starting from 1 to ");
+scanf("%d",&n);
+for(int i = 0 ; i<=n;i++)
+    {
+printf("the value of first n natural no. is %d\n",i);
+}
+return 0;
+}
