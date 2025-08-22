fb5e578a46494595a0fce2353cf7bb08b2ae31fc
Darshil Patel <darshil0806@gmail.com>
2025-08-22 19:33:55 +0530

    Add files via upload

---
+++ game.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator with current time
+    srand(time(0));
+
+    // Generate random number between 1 and 100
+    int randomNumber = (rand() % 100) + 1;
+    int no_of_guesses = 0;
+    int gussed;
+
+
+    //printf("Random number between 1 and 100: %d\n", randomNumber);
+    do {
+        printf("guess the number (between 1 and 100): ");
+        scanf("%d", &gussed);
+        if (gussed < randomNumber) {
+        printf("you gussed too low\n");
+    } else if (gussed > randomNumber) {
+        printf("you gussed too high\n");
+    } else {
+        printf("you gussed it right in %d attempts\n", no_of_guesses);
+    }
+    no_of_guesses++;
+    } while (gussed != randomNumber);
+    
+
+  
+
+    return 0;
+}
+
