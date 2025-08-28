ac21e363cbf7bc047ae7180ec90bcba720fffe6b
Darshil Patel <darshil0806@gmail.com>
2025-08-28 15:41:00 +0530

    Add files via upload

---
+++ vv.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+intmain (){
+    int a,b,c,d;
+    a=0;
+    for(i=0;i<=9;i++){
+        printf("entera randomno");
+        scanf("%d",&b);
+        if(b>a){
+            a=b;
+        }
+        else(b=b)
+    }
+    printf("the largest no. is%d",b);
+
+}
