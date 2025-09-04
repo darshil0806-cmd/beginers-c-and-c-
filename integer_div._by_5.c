9179bfe6922fb8c16377e7f25d1a6e72cd74d461
Darshil Patel <darshil0806@gmail.com>
2025-09-05 00:14:02 +0530

    Add files via upload

---
+++ integer_div._by_5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    
+    for(int i = 0; i <= 100; i++){
+        if(i % 5 == 0){
+            printf("%d is divisible by 5 \n",i);
+        }
+    }
+    
+    return 0;
+}
+
