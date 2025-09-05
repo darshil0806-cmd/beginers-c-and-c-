e470cf0b86177ace271062d8d9f11337796c0102
Darshil Patel <darshil0806@gmail.com>
2025-09-06 02:06:03 +0530

    Add files via upload

---
+++ reverse_digits.c
@@ -0,0 +1,21 @@
+// reverse digits
+
+#include <stdio.h>
+
+int main()
+{
+    int num, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("reversed digits: ");
+    while (num > 0)
+    {
+        digit = num % 10;
+        printf("%d", digit);
+        num = num / 10;
+    }
+
+    return 0;
+}
