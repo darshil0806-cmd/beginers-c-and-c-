397a29019c5f616326fc874c9508922b0618ee5e
Darshil Patel <darshil0806@gmail.com>
2025-09-11 15:32:00 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,19 @@
+// reverse digits
+
+#include <stdio.h>
+
+int main()
+{
+    int j,i;
+    for(int i = 1;i<=2;i++)
+        {
+        printf("%d",i);
+        for(int j = 1;j<=3;j++)
+            //{printf("%d",i);
+        printf("%d\n",j);
+
+    }
+    }
+
+    return 0;
+}
---
+++ Untitled2.c
@@ -0,0 +1,15 @@
+// reverse digits
+
+#include <stdio.h>
+
+int main()
+{
+    int j,i,c;
+    for(int i = 1;i<=5;i++)
+        {
+        printf("%d%d\n",i,6-i);
+
+    }
+
+    return 0;
+}
