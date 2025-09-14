7cc9a366dacab8b9387e9cec57e3f89413ed2279
Darshil Patel <darshil0806@gmail.com>
2025-09-14 11:15:19 +0530

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -7,13 +7,13 @@ int main()
     int j,i;
     for(int i = 1;i<=2;i++)
         {
-        printf("%d",i);
+        //printf("%d",i);
         for(int j = 1;j<=3;j++)
-            //{printf("%d",i);
+            {printf("%d",i);
         printf("%d\n",j);
 
     }
-    }
+    
 
     return 0;
-}
+}}
