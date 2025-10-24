9d93e26f7c987f7dfacfcab5a5c5f22c8c7504ae
Darshil Patel <darshil0806@gmail.com>
2025-10-24 14:29:36 +0530

    Add files via upload

---
+++ pattern_star_diamond.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter no. of lines : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+            printf("*");
+        }
+        for(int s=1;s<=i-1;s++){
+            printf("*");
+        }
+        printf("\n");
+    }for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=n-i-1;k++){
+            printf("*");
+        }
+        for(int s=1;s<=n-i;s++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
