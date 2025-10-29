5c0c0bb2b6270b6d5f21b042a7df5ab6ea8da1b9
Darshil Patel <darshil0806@gmail.com>
2025-10-29 16:46:43 +0530

    Add files via upload

---
+++ power_functions.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+include<math.c>
+
+int main(){
+int a;
+printf("enter a number:");
+scanf("%d",&a);
+printf("square of %d is %.2f\n",a,pow(a,2));
+return 0;
+}
---
+++ sqrt_functions.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int a;
+printf("enter a number:");
+scanf("%d",&a);
+printf("square root of %d is %.2f\n",a,sqrt(a));
+return 0;
+}
